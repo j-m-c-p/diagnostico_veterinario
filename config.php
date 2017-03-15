832ce85c22f82174dac479318f4d043a1dd834eb
j-m-c-p <jhonatancubi@gmail.com>
2017-03-15 07:53:20 -0500

    Create config.php
    Este archivo contiene la conexión de la base de datos.

---
+++ config.php
@@ -0,0 +1,18 @@
+<!--
+* 
+* versión: 1.0 
+* autores: Jhonnatan cubides, Harley santoyo
+*
+-->
+
+<?php 
+	$servidor = "localhost";
+    $usuario = "root";
+    $clave = "";
+    $bd = "bd_diagnostico_clinico";
+
+    //Puede colocar s o n minúsculas para ver diferentes impresiones de prueba de SQL y otras características.
+	 $sn_diagnostico_clinico = "s";
+	 $sn_escribir_log = "s";
+
+ ?>
